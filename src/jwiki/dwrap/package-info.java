9d9192950bee294410757f69cf543642268ac3a7
fastily <fastily@yahoo.com>
2015-08-26 22:59:44 -0700

    minor edit update json-java jar, documentation copyedits and clarifications

--- src/jwiki/dwrap/package-info.java
+++ src/jwiki/dwrap/package-info.java
@@ -1,5 +1,7 @@
 /**
  * Contains data storage formats for jwiki. These are simple, immutable objects with publicly exposed accessors.
+ * 
+ * @author Fastily
  */
 package jwiki.dwrap;
 
--- src/jwiki/util/package-info.java
+++ src/jwiki/util/package-info.java
@@ -3,6 +3,8 @@
  * available in their own package for general purpose use. Contains methods ranging from I/O, to String functions, to
  * GUI. There are also tools/frameworks such as FLogin/WikiGen (which provide an interface to locally save login
  * credentials and encrypt them).
+ *
+ * @author Fastily
  */
 
 package jwiki.util;
