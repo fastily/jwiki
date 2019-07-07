941739670a23ad88191abd7f7a072f46209d061b
fastily <fastily@users.noreply.github.com>
2019-07-07 02:33:09 -0700

    minor edit - bump okhttp version

--- src/main/java/fastily/jwiki/core/ApiClient.java
+++ src/main/java/fastily/jwiki/core/ApiClient.java
@@ -3,6 +3,7 @@ package fastily.jwiki.core;
 import java.io.IOException;
 import java.net.Proxy;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -55,7 +56,7 @@ class ApiClient
 		this.wiki = wiki;
 
 		OkHttpClient.Builder builder = new OkHttpClient.Builder().cookieJar(new JwikiCookieJar()).readTimeout(2, TimeUnit.MINUTES)
-				.protocols(List.of(Protocol.HTTP_1_1));
+				.protocols(Arrays.asList(Protocol.HTTP_1_1));
 		if (proxy != null)
 			builder.proxy(proxy);
 
@@ -142,7 +143,7 @@ class ApiClient
 		MultipartBody.Builder mpb = new MultipartBody.Builder().setType(MultipartBody.FORM);
 		form.forEach(mpb::addFormDataPart);
 
-		mpb.addFormDataPart("chunk", fn, RequestBody.create(octetstream, chunk));
+		mpb.addFormDataPart("chunk", fn, RequestBody.create(chunk, octetstream));
 
 		Request r = startReq(params).post(mpb.build()).build();
 		return client.newCall(r).execute();
